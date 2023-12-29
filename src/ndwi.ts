3284a75e984466b9ac1c553dbaba9fa18cbb39e1
bre <iwanttosignup321@gmail.com>
2023-12-29 23:43:05 +0800

    the opening code provided by chatgpt is not working

--- src/ndwi.ts
+++ src/ndwi.ts
@@ -9,19 +9,13 @@ const b3Input = document.querySelector('#b3') as HTMLInputElement;
 const b5Input = document.querySelector('#b5') as HTMLInputElement;
 const button = document.querySelector("#btn") as HTMLButtonElement;
 const rangeInput = document.querySelector("#range") as HTMLInputElement;
-const openingInput=document.querySelector("#opening") as HTMLInputElement;
-openingInput.onchange=((e)=>{
-    const openingNumber=parseInt(openingInput.value);  
-    const waterCanvas=document.getElementById('water') as HTMLCanvasElement;
-    //const newWaterCanvas=cloneCanvas
-
-})
+const openingInput = document.querySelector("#opening") as HTMLInputElement;
 
 rangeInput.onchange = ((e) => {
     const threshold = parseInt(rangeInput.value);
     const waterCanvas = document.getElementById('water') as HTMLCanvasElement;
     const ndwiCanvas = document.getElementById('ndwi') as HTMLCanvasElement;
-    const newWaterCanvas = water(ndwiCanvas, threshold*2.55);
+    const newWaterCanvas = water(ndwiCanvas, threshold * 2.55);
     newWaterCanvas.style.width = "35%";
     document.body.replaceChild(newWaterCanvas, waterCanvas);
     newWaterCanvas.id = "water";
@@ -49,39 +43,45 @@ button.onclick = async (e) => {
     ndwiCanvas.style.width = "20%";
     document.body.appendChild(ndwiCanvas);
     const waterCanvas = water(ndwiCanvas);
+    const originalWaterCanvas = cloneCanvas(waterCanvas);
     document.body.appendChild(waterCanvas);
     const histogramCanvas = histogram(data.data);
     //const copyhistogramCanvas = JSON.parse(JSON.stringify(histogramCanvas)) as HTMLCanvasElement;
-    const copyhistogramCanvas =cloneCanvas(histogramCanvas)
+    const copyhistogramCanvas = cloneCanvas(histogramCanvas)
     copyhistogramCanvas.id = "histogram";
-    const histogramOnClick=(e:MouseEvent)=>{
-        const newhistogramCanvas=cloneCanvas(histogramCanvas)
-        const x=e.offsetX;
-        const y=e.offsetY;
-        const ctx=newhistogramCanvas.getContext('2d')!;
+    const histogramOnClick = (e: MouseEvent) => {
+        const newhistogramCanvas = cloneCanvas(histogramCanvas)
+        const x = e.offsetX;
+        const y = e.offsetY;
+        const ctx = newhistogramCanvas.getContext('2d')!;
         ctx.beginPath();
-        ctx.moveTo(x,0);    
-        ctx.lineTo(x,150);
-        ctx.strokeStyle="red";
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, 150);
+        ctx.strokeStyle = "red";
         ctx.stroke();
-        newhistogramCanvas.id="histogram";
-        newhistogramCanvas.onclick=histogramOnClick;
-        (e.target!as HTMLCanvasElement).replaceWith(newhistogramCanvas);  
-        const threshold=x/20;
-        const ndwiCanvas=document.getElementById('ndwi') as HTMLCanvasElement;
-        const newWaterCanvas=water(ndwiCanvas,threshold*10);
-        const waterCanvas=document.getElementById('water') as HTMLCanvasElement;
+        newhistogramCanvas.id = "histogram";
+        newhistogramCanvas.onclick = histogramOnClick;
+        (e.target! as HTMLCanvasElement).replaceWith(newhistogramCanvas);
+        const threshold = x / 20;
+        const ndwiCanvas = document.getElementById('ndwi') as HTMLCanvasElement;
+        const newWaterCanvas = water(ndwiCanvas, threshold * 10);
+        const waterCanvas = document.getElementById('water') as HTMLCanvasElement;
         waterCanvas.replaceWith(newWaterCanvas)
     }
-    const openingOnChaneg=(e:Event)=>{
-        
+    const openingOnChaneg = (e: Event) => {
+        const newCanvas = openingCanvas(cloneCanvas(originalWaterCanvas), parseInt(openingInput.value));
+        console.log(newCanvas.data)
+        const waterCanvas = document.getElementById('water') as HTMLCanvasElement;
+        newCanvas.id = "water";
+        newCanvas.style.width = "35%";
+        waterCanvas.replaceWith(newCanvas);
     }
+    document.querySelector("#openingButton")!.onclick = openingOnChaneg;
 
 
-    
-    copyhistogramCanvas.onclick=histogramOnClick;
+    copyhistogramCanvas.onclick = histogramOnClick;
     document.body.append(copyhistogramCanvas);
-    
+
 
 
     //generateHistogram(ndwiCanvas)
@@ -102,6 +102,127 @@ Tiff.initialize({ TOTAL_MEMORY: 19777216 * 10 });
 
 
 
+/*
+const openingCanvas = (canvas: HTMLCanvasElement, openingNumber: number) => {
+    const context = canvas.getContext('2d');
+    if (!context) {
+        console.error('Unable to get 2D context from canvas');
+        return;
+    }
+
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    const width = canvas.width;
+    const height = canvas.height;
+
+    const getIndex = (x: number, y: number) => (y * width + x) * 4;
+
+    const isPixelValid = (x: number, y: number) => {
+        const index = getIndex(x, y);
+        return data[index] !== 0; // Check if the pixel is not 0
+    };
+
+    const isSurroundingZero = (x: number, y: number) => {
+        for (let i = -openingNumber; i <= openingNumber; i++) {
+            for (let j = -openingNumber; j <= openingNumber; j++) {
+                const nx = x + i;
+                const ny = y + j;
+                if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
+                    if (data[getIndex(nx, ny)] !== 0) {
+                        return false; // If any surrounding pixel is not 0, return false
+                    }
+                }
+            }
+        }
+        return true; // All surrounding pixels are 0
+    };
+
+    // Apply opening operation
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            if (isPixelValid(x, y) && isSurroundingZero(x, y)) {
+                // Set the pixel to 0
+                const index = getIndex(x, y);
+                data[index] = 0;
+                data[index + 1] = 0;
+                data[index + 2] = 0;
+                data[index + 3] = 0; // Alpha channel
+            }
+        }
+    }
+
+    // Update the canvas with the modified data
+    context.putImageData(imageData, 0, 0);
+    return canvas;
+};*/
+
+const openingCanvas = (sourceCanvas: HTMLCanvasElement, openingNumber: number): HTMLCanvasElement => {
+    const sourceContext = sourceCanvas.getContext('2d');
+    if (!sourceContext) {
+      console.error('Unable to get 2D context from source canvas');
+      return sourceCanvas;
+    }
+  
+    const sourceImageData = sourceContext.getImageData(0, 0, sourceCanvas.width, sourceCanvas.height);
+    const sourceData = sourceImageData.data;
+  
+    const width = sourceCanvas.width;
+    const height = sourceCanvas.height;
+  
+    const getIndex = (x: number, y: number) => (y * width + x) * 4;
+  
+    const isPixelValid = (x: number, y: number) => {
+      const index = getIndex(x, y);
+      return sourceData[index] !== 0;
+    };
+  
+    const isSurroundingZero = (x: number, y: number) => {
+      for (let i = -openingNumber; i <= openingNumber; i++) {
+        for (let j = -openingNumber; j <= openingNumber; j++) {
+          const nx = x + i;
+          const ny = y + j;
+          if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
+            if (sourceData[getIndex(nx, ny)] !== 0) {
+              return false;
+            }
+          }
+        }
+      }
+      return true;
+    };
+  
+    // Create a new canvas
+    const resultCanvas = document.createElement('canvas');
+    resultCanvas.width = width;
+    resultCanvas.height = height;
+    const resultContext = resultCanvas.getContext('2d');
+    if (!resultContext) {
+      console.error('Unable to get 2D context for result canvas');
+      return sourceCanvas;
+    }
+  
+    // Apply opening operation
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        if (isPixelValid(x, y) && isSurroundingZero(x, y)) {
+          // Set the pixel to 0 in the result canvas
+          const index = getIndex(x, y);
+          sourceData[index] = 0;
+          sourceData[index + 1] = 0;
+          sourceData[index + 2] = 0;
+          sourceData[index + 3] = 0;
+        }
+      }
+    }
+  
+    // Draw the modified data onto the result canvas
+    resultContext.putImageData(sourceImageData, 0, 0);
+  
+    // Return the new canvas
+    return resultCanvas;
+  };
+
 
 
 
@@ -118,7 +239,7 @@ const fileToCanvas = (file: File): Promise<HTMLCanvasElement> => {
     }, (error) => {
         reject(error);
         alert("change other data");
-        
+
     })
 }
 
@@ -148,13 +269,13 @@ const water = (ndwiCanvas: HTMLCanvasElement, threshold = 20) => {
         }
     }
     waterContext.putImageData(waterData, 0, 0)
-    waterCanvas.id="water";
-    waterCanvas.style.width="35%";
+    waterCanvas.id = "water";
+    waterCanvas.style.width = "35%";
     return waterCanvas;
 }
-const waterRust = (ndwiCanvas: HTMLCanvasElement,threshold=0.2) => {
+const waterRust = (ndwiCanvas: HTMLCanvasElement, threshold = 0.2) => {
     //const 
-    
+
 }
 
 
